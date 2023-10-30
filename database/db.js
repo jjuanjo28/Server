7ad2764190372b05aee5d7da40c7acc112200288
jjuanjo28 <105110146+jjuanjo28@users.noreply.github.com>
2023-10-29 21:18:52 -0300

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,10 +1,17 @@
 import { Sequelize } from "sequelize";
+import {
+  DB_HOST,
+  DB_NAME,
+  DB_PASSWORD,
+  DB_USER,
+  DB_PORT
+} from "../src/config.js"
 
-const db = new Sequelize("prestamos", "root", "", {
-  host: "localhost",
+const db = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
+  host: DB_HOST,
   dialect: "mysql",
-  port: 3307,
-   define: {
+  port: DB_PORT,
+  define: {
     timestamps: false,
   },
 });
